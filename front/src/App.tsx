1ecf565972e547bbf26999ab1cb02392fe99ddb8
Kirill Salykin <kirill.salykin@gmail.com>
2024-09-06 11:17:37 +0200

    Add form and dummy mutation

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,23 +1,77 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { useQuery } from 'urql';
 
-const DUMMY_QUERY = `
+import React, { useRef, useState } from "react";
+import { useQuery, useMutation } from 'urql';
+
+const dummyQuery = `
 query {
   dummy {
     add(a: 5, b: 10)
   }
 }
-`
+`;
+
+const dummyMutationQ = `
+mutation ($a: Int!, $b: Int!) {
+    dummy {
+      add(a: $a, b: $b)
+    }
+  }
+`;
+
+
+const NumberForm: React.FC = () => {
+  const aRef = useRef<HTMLInputElement>(null);
+  const bRef = useRef<HTMLInputElement>(null);
+
+  const [result, setResult] = useState<number | null>(null);
+  const [dummyMutationResult, dummyMutation] = useMutation(dummyMutationQ);
+
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const a = aRef.current?.valueAsNumber || 0;
+    const b = bRef.current?.valueAsNumber || 0;
+
+    const { data, error } = await dummyMutation({ a, b });
+
+    if (data && data.dummy && data.dummy.add !== undefined) {
+      setResult(data.dummy.add);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="a">Number A:</label>
+          <input id="a" type="number" ref={aRef} />
+        </div>
+        <div>
+          <label htmlFor="b">Number B:</label>
+          <input id="b" type="number" ref={bRef} />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+
+      {result !== null && (
+        <div>
+          <h2>Result: {result}</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
 function App() {
-   const [result, reexecuteQuery] = useQuery({
-    query: DUMMY_QUERY,
-  });
+    const [result, reexecuteQuery] = useQuery({
+     query: dummyQuery,
+   });
 
   const { data, fetching, error } = result;
 
-  console.log("data", data);
   return (
     <div className="App">
       <header className="App-header">
@@ -25,6 +79,9 @@ function App() {
         <p>
         {data.dummy.add}
         </p>
+
+        <NumberForm/>
+
         <a
           className="App-link"
           href="https://reactjs.org"

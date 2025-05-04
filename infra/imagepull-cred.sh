de7d24504f9008e80682e8e3ec4d953b6fd3c101
Kirill Salykin <kirill.salykin@gmail.com>
2025-05-04 15:24:05 +0200

    Duplicate imagepull-cred

--- infra/imagepull-cred.sh
+++ infra/imagepull-cred.sh
@@ -2,14 +2,22 @@
 set -euo pipefail
 
 : "${DOCKERHUB_USER:?Need to set DOCKERHUB_USER}"
-: "${DOCKERHUB_TOKEN:?Need to set DOCKERHUB_PASS}"
+: "${DOCKERHUB_TOKEN:?Need to set DOCKERHUB_TOKEN}"
 
 AUTH=$(printf "%s:%s" "$DOCKERHUB_USER" "$DOCKERHUB_TOKEN" | base64 -w0)
 DOCKER_CFG_JSON=$(printf '{"auths":{"https://index.docker.io/v1/":{"auth":"%s"}}}' "$AUTH")
 
 kubectl create secret generic "imagepull-cred" \
-  --namespace= "default" \
+  --namespace "default" \
   --type=kubernetes.io/dockerconfigjson \
   --from-literal=.dockerconfigjson="$DOCKER_CFG_JSON" \
   --dry-run=client -o yaml \
 | kubectl apply -f -
+
+kubectl create secret generic "imagepull-cred" \
+  --namespace "flux-system" \
+  --type=kubernetes.io/dockerconfigjson \
+  --from-literal=.dockerconfigjson="$DOCKER_CFG_JSON" \
+  --dry-run=client -o yaml \
+| kubectl apply -f -
+

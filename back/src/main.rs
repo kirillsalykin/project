ebd9380477c503e6d13ca41c0f96825a76f9d485
Kirill Salykin <kirill.salykin@gmail.com>
2025-05-04 15:02:24 +0200

    Use yaml

--- back/src/main.rs
+++ back/src/main.rs
@@ -4,7 +4,7 @@ use project::configuration::Config;
 #[tokio::main]
 async fn main() {
     let config = config::Config::builder()
-        .add_source(config::File::with_name("config.yml"))
+        .add_source(config::File::with_name("config.yaml"))
         .add_source(config::Environment::default().separator("_"))
         .build()
         .unwrap()
